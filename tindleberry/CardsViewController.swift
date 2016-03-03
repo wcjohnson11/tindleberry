ea33a18600548001c9972cac6bb6573f32a7419d
Isis Anchalee <isiswenger@gmail.com>
2016-03-02 19:21:14 -0800

    Complete second milestone

--- tindleberry/CardsViewController.swift
+++ tindleberry/CardsViewController.swift
@@ -9,9 +9,29 @@
 import UIKit
 
 class CardsViewController: UIViewController {
-
+    @IBOutlet var parentView: UIView!
+    var gestureBeginPt: CGPoint?
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBAction func onPhotoPanned(panGestureRecognizer: UIPanGestureRecognizer) {
+        if panGestureRecognizer.state == UIGestureRecognizerState.Began {
+            gestureBeginPt = profileImageView.center
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
+            let translation = panGestureRecognizer.translationInView(parentView)
+            profileImageView.center.x = translation.x
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
+            
+        }
+    }
+    
+    func setupImages(){
+        profileImageView.image = UIImage(named: "ryan")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupImages()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
